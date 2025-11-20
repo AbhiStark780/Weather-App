5d5a2566327277390b90989a1b2a98ac092448bd
Abhishek Tipiria <abhistark57@gmail.com>
2025-11-21 01:10:33 +0530

    Add 5 day forecast feature

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ A simple, robust Command Line Interface (CLI) application that fetches real-time
 
 ## 🚀 Features
 * **Real-time Data:** Fetches current temperature, humidity, wind speed, and weather conditions.
+* **5-Day Forecast:** Displays a clean, formatted summary of the weather for the next 5 days (noon snapshots).
 * **Visual Feedback:** Uses dynamic emojis (☀️, 🌧️, ❄️) based on weather conditions.
 * **User Friendly:** Handles typos and errors gracefully without crashing.
 * **Interactive:** Runs in a loop until you decide to quit.
 
 ## 🛠️ Tech Stack
 * **Language:** Python 3.11.5
-* **Libraries:** `requests`
+* **Libraries:** `requests`, `datetime` (Standard Library)
 * **API:** OpenWeatherMap
 
 ## ⚙️ Setup & Installation
@@ -41,13 +42,20 @@ A simple, robust Command Line Interface (CLI) application that fetches real-time
 ## 📸 Usage Example
 
 ```text
-Enter a city name: London
-
+Enter a city name: Las Vegas
 ----------------------------------------
-Weather Report for London
+Weather Report for Las Vegas
 ----------------------------------------
-🌧️   light rain
-🌡️   Temperature: 15.32°C
-💧   Humidity: 82%
-💨   Wind: 4.12 m/s
+☀️   clear sky
+🌡️   Temperature: 13.43°C
+💧   Humidity: 57%
+💨   Wind: 1.54m/s
 ----------------------------------------
+5 Day Weather Forecast
+----------------------------------------
+Fri, 21 Nov    🌧️      7.61°C     - light rain
+Sat, 22 Nov    🌧️      10.74°C    - light rain
+Sun, 23 Nov    ☁️      12.74°C    - few clouds
+Mon, 24 Nov    ☀️      11.44°C    - clear sky
+Tue, 25 Nov    ☀️      11.57°C    - clear sky
+Check another city ? (Y/N): 
--- weather.py
+++ weather.py
@@ -2,9 +2,14 @@
 import requests
 # Getting the secret API Key without directly embedding it the main file
 from config import apiKey
+# Module to handle Date and Time 
+from datetime import datetime
 
 # List of Yes Options 
 YES_OPTIONS = ["y","yes"]
+
+# Time for which i am looking the forecast data
+FORECAST_TIME = "12:00:00"
 
 # Emoji for showing the weather visuals
 weather_emoji = {
@@ -16,7 +21,7 @@ weather_emoji = {
     }
 
 # Create the get_weather function to handle the api call
-def get_weather(city, api_key):
+def get_weather(city, API_key):
     """
         Fetches the weather data from OpenWeather API
 
@@ -28,7 +33,7 @@ def get_weather(city, api_key):
             dict: weather data if successfull, otherwise None
     """
     # Create the url variable (using the city and api_key parameters)
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units=metric"
     # Get the response
     response = requests.get(url)
     # If the status code is 200, return the response.json().
@@ -37,6 +42,54 @@ def get_weather(city, api_key):
         return response.json()
     else:   
         return None
+
+def get_forecast(city, API_key):
+    """
+        Fetches the weather forecast data from OpenWeather API
+
+        Args:
+            city (string): The city to look up
+            api_key (string): Your Personal API Key
+
+        Return:
+            dict: forecast weather data if successfull, otherwise None
+    """
+    # created the url variable that stores the api end-point
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_key}&units=metric"
+    # get the response
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+    
+# function for displaying the forecast data
+def display_forecast(forecast_data):
+    """
+        Display the 5 Days weather forecast
+
+        Args: 
+            forecast_data (dict): Extract data from this dictionary
+
+        Return:
+            Nothing to return, just display the forecast data upon calling
+    """
+    # itereate through each forecast data
+    print("-" * 40)
+    print("5 Day Weather Forecast")
+    print("-" * 40)
+    daily_forecast = forecast_data['list']
+    for forecast in daily_forecast:
+        if FORECAST_TIME in forecast['dt_txt']:
+            date = datetime.strptime(forecast['dt_txt'][:10], "%Y-%m-%d") # Here we are creating a date object, slicing the dt_txt as we need only the date and not the time 
+            temperature = forecast['main']['temp']
+            visual_temp = str(temperature) + "\u00b0C"
+            description = forecast['weather'][0]['description']
+            print(f"{date.strftime('%a, %d %b'):<15}",end="") # strftime helps in formating the date object just as we need i.e Fri, 21 Nov
+            print(f"{weather_emoji.get(forecast['weather'][0]['main'], '⚪'):<5}   {visual_temp:<10} - {description:<15}")
+
+
 
 def main():
     while True:
@@ -63,10 +116,15 @@ def main():
             # Display the Wind Speed(m/s)
             print("💨", end = "   ")
             print(f"Wind: {weather_data['wind']['speed']}m/s")
-            print("-" * 40)
+
+            # Get the Forecast Data
+            forecast_data = get_forecast(city_name, apiKey)
+            # Display the Forecast Data
+            display_forecast(forecast_data)
         else:
             print("City Not Found.")
 
+        # Prompt the user to check for another city
         user_answer = input("Check another city ? (Y/N): ")
 
         if user_answer.lower() in YES_OPTIONS:
